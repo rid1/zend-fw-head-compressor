867c037ee6f76ae617d1dfbd5eb0a611360d2162
Alexey S. Kachayev <kachayev@gmail.com>
2010-11-24 16:02:20 +0200

    Simple test suites for scripts compressor

--- tests/library/Dm/View/Helper/CompressScriptTest.php
+++ tests/library/Dm/View/Helper/CompressScriptTest.php
@@ -12,8 +12,58 @@
 class Dm_View_Helper_CompressScriptTest
     extends DmTestCase
 {
-    public function testUnitWorksNormaly()
+    /**
+     * @var Dm_View_Helper_CompressScript
+     */
+    protected $_helper = null;
+
+    /**
+     * New helper object for each test case
+     */
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->_helper = new Dm_View_Helper_CompressScript();
+        $this->_helper->setView(new Zend_View());
+    }
+
+    public function testGetOptionShouldReturnActualValue()
+    {
+        $this->assertTrue($this->_helper->getOption('combine'));
+    }
+    
+    public function testGetOptionWithNonExistenKeyShouldReturnDefaultValue()
+    {
+        $this->assertEquals(50, $this->_helper->getOption('key-which_don`t-exists', 50));
+    }
+
+    public function testSetConfigShouldOverwriteDefaultConfiguration()
+    {
+        $this->_helper->setConfig(array('compress'=>false));
+        $this->_assertOverwritedCompressOption();
+    }
+    
+    public function testSetConfigShouldUnderstandConfigObject()
+    {
+        $this->_helper->setConfig(new Zend_Config(array('compress'=>false)));
+        $this->_assertOverwritedCompressOption();
+    }
+
+    protected function _assertOverwritedCompressOption()
+    {
+        $this->assertFalse($this->_helper->getOption('compress'), 'Overwrited value');
+        $this->assertTrue($this->_helper->getOption('combine'), 'Default value'); 
+    }
+
+    public function testSearchJsFileShouldReturnNullForNonExisten()
+    {
+        $this->assertEquals(null, $this->_helper->searchJsFile('file-which-never-be'));
+    }
+
+    public function testCompressScriptWithConfigParamShouldOverwriteConfig()
+    {
+        $this->_helper->compressScript(new Zend_Config(array('compress'=>false, 'combine'=>false)));
+        $this->assertFalse($this->_helper->getOption('compress'));
     }
 }
